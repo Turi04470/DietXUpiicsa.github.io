4e515b9287620e22d5c9418b4692f1280cd29659
Turi04470 <darkshadow04480@gmail.com>
2023-06-21 17:15:49 -0600

    super correcion

--- JS/login_and_signup.js
+++ JS/login_and_signup.js
@@ -1,8 +1,8 @@
-var anima_D = document.getElementById("D");
-var anima_I = document.getElementById("I");
-var anima_E = document.getElementById("E");
-var anima_T = document.getElementById("T");
-var anima_X = document.getElementById("X");
+let anima_D = document.getElementById("D");
+let anima_I = document.getElementById("I");
+let anima_E = document.getElementById("E");
+let anima_T = document.getElementById("T");
+let anima_X = document.getElementById("X");
 
 anima_D.addEventListener("animationend", D_end, false);
 anima_I.addEventListener("animationend", I_end, false);
@@ -25,5 +25,15 @@ function T_end() {
 }
 function X_end() {
     anima_X.classList.remove('opacity');
+}
+
+let button = document.querySelector('.button_style_3');
+
+// Agregar un event listener al botón
+button.addEventListener('click', redirectToIndex);
+
+// Función que redirige a la página principal
+function redirectToIndex() {
+    window.location.href = "../Index.html";
 }
 
